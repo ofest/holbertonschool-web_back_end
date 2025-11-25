acc39ae3737d78e2c025ec59b3ab06fd6463bc1e
ofest <dars20@hotmail.com>
2025-11-25 15:25:56 +0100

    Feat:functio that multiplies

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,7 +3,7 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier : float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Attributes: multiplier : float as argument
 
