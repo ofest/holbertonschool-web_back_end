33e78365af1758ca400d911ebf0d7ac8dedd4645
ofest <dars20@hotmail.com>
2025-11-26 14:33:51 +0100

    Feat: show the delay of time in ascending orders.

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module containing task_wait_random"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_random(n: int, max_delay: int) -> List[float]:
+    """
+    Run task_wait_random n times and return the delays in ascending order.
+    """
+    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
+
+    output = []
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        output.append(result)
+
+    return output
