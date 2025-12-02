86fbf615faf2fa18072c0b6fbc03dc7f0802ac23
ofest <dars20@hotmail.com>
2025-12-02 10:49:54 +0100

    Feat: retrieve a specific segment of the dataset while managing deletions effectively.

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int):
+    """
+    Return a tuple of (start_index, end_index) for the given
+    page and page_size (zero-based, end is exclusive).
+    Example: page=1, page_size=10 -> (0, 10)
+             page=2, page_size=10 -> (10, 20)
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return start, end
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+        if start >= len(dataset):
+            return []
+
+        return dataset[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10):
+        data = self.get_page(page, page_size)
+
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages,
+        }
