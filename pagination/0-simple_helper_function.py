e70175ef1a2acbd4d784d9645f031fd0d6ab33c4
ofest <dars20@hotmail.com>
2025-12-02 09:44:17 +0100

    Feat: Function that return the tuple.

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+takes two integer arguments
+"""
+
+def index_range(page, page_size):
+    """
+    function should return a tuple of size two
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
---
+++ pagination/README.md
@@ -0,0 +1,9 @@
+Exercises of pagination
+
+0. Simple helper function
+
+1. Simple pagination
+
+2. Hypermedia pagination
+
+3. Deletion-resilient hypermedia pagination
