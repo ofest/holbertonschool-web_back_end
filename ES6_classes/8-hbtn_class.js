a152aaf3c58e07c0ca4560ea40f4c45466a8e1e5
ofest <dars20@hotmail.com>
2025-12-12 16:08:06 +0100

    Feat: Implementing a class.

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,20 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') return this._size;
+    if (hint === 'string') return this._location;
+    return this._size;
+  }
+}
