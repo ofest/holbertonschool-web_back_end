43c0d5dd23aee84171d6b5579c6da935f98a75ec
ofest <dars20@hotmail.com>
2025-12-16 15:20:47 +0100

    Feat: Function that returns a specific city with their new grade.

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const gradeObj = newGrades.find((grade) => grade.studentId === student.Id);
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
       return {
         ...student,
         grade: gradeObj ? gradeObj.grade : 'N/A',
