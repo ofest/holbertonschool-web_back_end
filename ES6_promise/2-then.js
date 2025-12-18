2804e677f3473e5d42f9e6f41601ffd16f56aa48
ofest <dars20@hotmail.com>
2025-12-18 13:45:08 +0100

    Feat: Got a reponse from the API.

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromApi(promise) {
   return promise
     .then((data) => {
-      console.log('Got a repoponse from the API');
+      console.log('Got a response from the API');
       return {
         status: 200,
         body: 'succes',
