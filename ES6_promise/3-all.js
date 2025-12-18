3d016c228bdbdca9925b80596a459172bca993f5
ofest <dars20@hotmail.com>
2025-12-18 14:02:41 +0100

    Feat: Handle multiple promises.

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(
+        `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`,
+      );
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
