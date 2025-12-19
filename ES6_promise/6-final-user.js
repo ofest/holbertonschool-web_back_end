a975e5048e3d3ec40544c21ce117354a701fed2c
ofest <dars20@hotmail.com>
2025-12-19 13:59:31 +0100

    Feat: multiple promises.

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,16 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return {
-        status: result.status,
-        value: result.value,
-      };
-    }
-    return {
-      status: result.status,
-      value: result.reason,
-    };
-  }));
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled'
+      ? result.value
+      : result.reason,
+  })));
 }
