1323b510dcd2d33b21d81eb21a02560696f0f448
ofest <dars20@hotmail.com>
2025-12-19 13:58:30 +0100

    Feat: multiple promises.

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,20 +1,20 @@
-import { signUpUser} from "4-user-Promise.js";
-import { uploadPhoto } from "5-photo-reject.js";
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(firstName),
-    ]).then ((result) => {
-        if(result.status === 'fulfilled') {
-            return{
-                status: result.status,
-                value: result.value
-            };
-        }
-        return {
-            status: result.status,
-             value: result.reason,
-        }
-    })
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: result.status,
+        value: result.value,
+      };
+    }
+    return {
+      status: result.status,
+      value: result.reason,
+    };
+  }));
 }
